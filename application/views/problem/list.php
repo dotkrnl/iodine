9b6e3144dd3f7d3d7bb3bedd786527940ed06460
Jason Lau <i@dotkrnl.com>
2014-03-22 21:39:48 +0800

    use same coloring method for problem list

--- application/views/problem/list.php
+++ application/views/problem/list.php
@@ -12,15 +12,17 @@
         <th class="pid"><?php echo(__('problem.list.id')); ?></th>
         <th class="ptitle"><?php echo(__('problem.list.title')); ?></th>
         <th class="pratio"><?php echo(__('problem.list.ratio_ac_submit')); ?></th>
+        <th class="colorcol"></th>
     </tr>
 </thead>
 <tbody>
 <?php /* @var Model_Problem[] $problemlist */ ?>
 <?php foreach($problemlist as $problem): ?>
     <tr href="<?php e::url("/problem/show/{$problem->problem_id}"); ?>" class="clickable">
-        <td class="<?php echo e::pass_status($problem);?> pid"><?php echo($problem->problem_id); ?></td>
+        <td class="pid"><?php echo($problem->problem_id); ?></td>
         <td class="ptitle"><?php echo HTML::anchor("/problem/show/{$problem->problem_id}", $problem->title);?></td>
         <td class="pratio"><?php echo($problem->accepted); ?>/<?php echo($problem->submit); ?></td>
+        <td class="colorcol <?php echo e::pass_status($problem);?>"></td>
     </tr>
 <?php endforeach;?>
 </tbody>
