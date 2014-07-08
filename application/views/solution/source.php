92ba1d41021a2e4bebe85eb9583374afb61cf375
Jason Lau <i@dotkrnl.com>
2014-07-08 23:31:13 +0800

    use large modal for errors

--- application/views/solution/source.php
+++ application/views/solution/source.php
@@ -3,7 +3,7 @@
 
 <?php if ($cinfo || $rinfo): ?>
 <div class="modal fade" id="error_info" tabindex="-1" role="dialog" aria-labelledby="error_info_label" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
       <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only"><?php echo(__('solution.source.error.close')); ?></span></button>
